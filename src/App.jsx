8337dfe3ef6b2d4ad956c0b2a5c4979ac9d9f7a3
Lopez Delgado Jose Andres <isc17350296@gmail.com>
2022-06-20 09:39:18 -0500

    tercer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Saludo from "./Saludo"
+
+const App =() => {
+  return (
+    <div className="App">
+      <Saludo />
+      <Saludo />
+      <Saludo />   
+      <Saludo />
+      <Saludo />
+      <Saludo /> 
+    </div>
+  );
+
+}
+  
+
+
+export default App;
---
+++ src/Saludo.jsx
@@ -0,0 +1,9 @@
+const Saludo = () => {
+    return (
+        <div>
+            <h1>Andres</h1>
+            Edad:23
+        </div>
+    );
+};
+export default Saludo; 
